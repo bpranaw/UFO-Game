61ca0ea4f3ec50cb9bfcfaea75c215a7a78b4a2f
Pranaw Bajracharya <bpranaw@gmail.com>
2020-08-25 22:00:43 -0700

    Added completed UFO practice game
    Completed the UFO practice game as tutorial guided. Copied all of the files to this local repository and pushing to master.

---
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public Text countText;
+	public Text WinText;
+	
+	private Rigidbody2D rigid_body_2d;
+	
+	private int count;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid_body_2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		SetCountText();
+    }
+
+    //Before Physics Occurs
+    void FixedUpdate()
+    {
+		float horizontal_movement = Input.GetAxis("Horizontal");
+		float vertical_movement = Input.GetAxis("Vertical");
+		
+		Vector2 movement = new Vector2(horizontal_movement, vertical_movement);
+		
+		rigid_body_2d.AddForce(movement * speed);
+	
+		
+    }
+	
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		
+		if(other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			
+			SetCountText();
+			
+			if(count >= 12)
+			{
+				WinText.text = "You Win!!!";
+			}
+		}
+	}
+	
+	void SetCountText()
+	{
+		countText.text = "Points: " + count.ToString();
+	}
+}
+
